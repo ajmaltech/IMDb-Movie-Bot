4b23d955e4b09e63042b9b13e66fbde98cac3eee
ajmaltech <83828598+ajmaltech@users.noreply.github.com>
2022-10-02 19:56:47 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -51,7 +51,7 @@ async def start(client, message):
             InlineKeyboardButton('🕵️ Search here Movie 🕵️', switch_inline_query_current_chat='')
             ],[
             InlineKeyboardButton('⭕️ Updates', url='https://t.me/josprojects'),
-            InlineKeyboardButton('Movie Club ⭕️', url='https://t.me/+y53tWFUw6Q43NzE9')
+            InlineKeyboardButton('Movie World ⭕️', url='https://t.me/+qkfA4zE_lSIwOTU1')
             ],[
             InlineKeyboardButton('❌ Close the Menu ❌', callback_data='close_data')
         ]]
@@ -99,7 +99,7 @@ async def start(client, message):
             InlineKeyboardButton('🕵️ Search here Movie 🕵️', switch_inline_query_current_chat='')
             ],[
             InlineKeyboardButton('⭕️ Updates', url='https://t.me/josprojects'),
-            InlineKeyboardButton('Movie Club ⭕️', url='https://t.me/+y53tWFUw6Q43NzE9')
+            InlineKeyboardButton('Movie World ⭕️', url='https://t.me/+qkfA4zE_lSIwOTU1')
             ],[
             InlineKeyboardButton('❌ Close the Menu ❌', callback_data='close_data')
         ]]
@@ -250,11 +250,11 @@ async def start(client, message):
         f_caption = f"{files.file_name}"
     buttons = [
         [
-            InlineKeyboardButton('⭕️ Support', url='https://t.me/JOSPSupport'),
-            InlineKeyboardButton('Channel ⭕️', url='https://t.me/josprojects/221')
+            InlineKeyboardButton('⭕️ Support', url='https://t.me/movieworldshsh'),
+            InlineKeyboardButton('Channel ⭕️', url='https://t.me/movieworldshsh')
         ],
         [
-            InlineKeyboardButton('🎬 Series & Movie Club 🎬', url=f'https://t.me/+y53tWFUw6Q43NzE9')
+            InlineKeyboardButton('🎬 Movie World 🎬', url=f'https://t.me/+qkfA4zE_lSIwOTU1')
         ]
         ]
     await client.send_cached_media(
